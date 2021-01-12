fe27d79449675f58e42dda1efe995cbdd7861510
Andrew Collier <andrew@exegetic.biz>
2021-01-12 09:47:54 +0200

    Cater for province in URL

--- cdsm-topo-id.sh
+++ cdsm-topo-id.sh
@@ -2,9 +2,12 @@
 
 # Directory of maps at http://www.madmappers.com/browse.php?TY=R.
 
-FOLDER=data/maps/za/topographic
+FOLDER=data/maps/topographic
 
-BASEURL="https://www.globalmapper.com/datasets/R/South%20Africa/sgSA50k/KZN/"
+BASEURL="https://www.globalmapper.com/datasets/R/South%20Africa/sgSA50k"
+
+https://www.globalmapper.com/datasets/R/South%20Africa/sgSA50k/Eastern%20Cape/3025CB.zip?
+https://www.globalmapper.com/datasets/R/South%20Africa/sgSA50k/Free%20State/2726BB.zip?
 
 mkdir -p $FOLDER
 
@@ -15,14 +18,24 @@ do
   echo "* $filename"
 
   filename=${filename}.zip
-  filepath=$FOLDER/$filename
-  
+  filepath=$FOLDER/$(basename "$filename")
+
+  url=$BASEURL/$filename
+  #
+  # URL encode spaces.
+  #
+  url=$(echo $url | sed 's/ /%20/')
+
+  # echo $filepath
+  # echo $url
+
   if test -f "$filepath"
   then
     echo "  - $filepath already exists."
   else
     echo "  - $filepath doesn't exist."
-    wget -q -O $filepath $BASEURL/$filename
+    echo "  - Downloading $url."
+    wget -q -c -O $filepath $url
     if [ $? -eq 0 ]; then
         echo "  - ✅ Download success."
     else
